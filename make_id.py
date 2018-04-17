b49e962bf5889ec972cd8a4e4e1127c321be0931
ivliev123 <ivliev123@mail.ru>
2018-04-17 14:24:00 +0300

    --all

--- make_id.py
+++ make_id.py
@@ -1,11 +1,25 @@
+# python3 reklama.py  -f 'da.pickle' -m 'w' -t 'ID.txt'
+
 import pickle
+import argparse
 import dlib
 from skimage import io
 from scipy.spatial import distance
 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--filefrom",type=str, default="",
+	help="использование pi камеры")
+ap.add_argument("-t", "--fileto", type=str, default="ID.txt",
+	help="выбор файла")
+ap.add_argument("-m", "--metod", type=str, default="w",
+	help="выбор- открытие файла для чтения, записи, дозаписи")
+args = vars(ap.parse_args())
+
+
 ID=0
 
-with open("da.pickle", "rb") as f:
+with open(args["filefrom"], "rb") as f:
     array_data = []
     try:
         while True:
@@ -35,7 +49,11 @@ for i in range(len(array_data)):
 				array_data[k+i+1][1]=array_data[i][1]
 				print('ID',array_data[k+i+1][1])
 
-
+d = open(args["fileto"], args["metod"])
 for i in range(len(array_data)):
+	s= str(array_data[i][1]) +"  "+ array_data[i][2] +"  " + array_data[i][3] 
+	d.write(s+ '\n')
 	print()
 	print(array_data[i])
+
+d.close()
