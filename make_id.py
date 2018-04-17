9ea9ecf2ffb61c8cae285f2d538029ff34deee9b
ivliev123 <ivliev123@mail.ru>
2018-04-17 14:26:07 +0300

    --all

--- make_id.py
+++ make_id.py
@@ -37,14 +37,13 @@ for i in range(len(array_data)):
 	if (array_data[i][1]=="none"):
 		array_data[i][1]=ID
 		ID=ID+1
-	print('i= ',i)
+
 	for k in range(len(array_data)-i-1):
 		if (array_data[k+i+1][1]=="none"):
 			dist=distance.euclidean(array_data[i][4], array_data[k+i+1][4])
 			array_data[i+k+1][5]=dist
-			print (dist)
+
 			
-			print()
 			if (dist<=0.5 	):
 				array_data[k+i+1][1]=array_data[i][1]
 				print('ID',array_data[k+i+1][1])
@@ -53,7 +52,6 @@ d = open(args["fileto"], args["metod"])
 for i in range(len(array_data)):
 	s= str(array_data[i][1]) +"  "+ array_data[i][2] +"  " + array_data[i][3] 
 	d.write(s+ '\n')
-	print()
-	print(array_data[i])
+
 
 d.close()
