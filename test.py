3c6058031de86a06a16fe5121006d79dac684fca
ivliev123 <123ivliev123@mail.ru>
2018-05-26 04:30:37 +0300

    --all

--- test.py
+++ test.py
@@ -27,8 +27,12 @@ ap.add_argument("-m", "--metod", type=str, default="w",
 args = vars(ap.parse_args())
 
 
-ID=0
+
 faceCount=0
+#кончный масив с конечной обработаной информацией
+array_finish_list=[]
+
+ID=0		
 
 with open(args["filefrom"], "rb") as f:
 	array_data = []
@@ -38,53 +42,47 @@ with open(args["filefrom"], "rb") as f:
 				
 	except (EOFError, pickle.UnpicklingError):
 		pass
-#for i in range(len(array_data)):
-#	print(array_data[i][7])
+
 i=0
 faceList = []
 
 while i < len(array_data):
 	
-	print(len(faceList),array_data[i][9])
+	deltat=[[0]*array_data[i][9]]*len(faceList) 
+	for tt in range(len(faceList)):
+		for kk in range(array_data[i][9]):
+			dt=array_data[i+kk][5]-faceList[tt][5]
+			deltat[tt][kk]=dt
+	tt=0
+	while tt <(len(faceList)):
+		n=0
+		for kk in range(array_data[i][9]):
+			
+			if deltat[tt][kk]>2:
+				n+=1
+		if n==array_data[i][9]:
+			del faceList[tt]		
+		tt+=1
+	
+
 #1
 	if(len(faceList) == 0):
-		print('1-----------------------------1')
+
 		for k in range(array_data[i][9]):
 			array_data[i+k][10]=faceCount
-			faceList.append(array_data[i+k])
-			#faceList[k][10]=faceCount
+			faceList.append(copy.deepcopy(array_data[i+k]))
 			faceCount += 1
-		#i+=array_data[i][9]
 
 
-		
 #2
 	if(len(faceList) <= array_data[i][9]):
 
-		print('2-----------------------------2')
-		deltat=[[0]*array_data[i][9]]*len(faceList) 
-		for tt in range(len(faceList)):
-			for kk in range(array_data[i][9]):
-				dt=array_data[i+kk][5]-faceList[tt][5]
-				deltat[tt][kk]=dt
-		tt=0
-		while tt <(len(faceList)):
-			n=0
-			for kk in range(array_data[i][9]):
-				
-				if deltat[tt][kk]>2:
-					n+=1
-			if n==array_data[i][9]:
-				del faceList[tt]		
-			tt+=1
 
 		metka=[False]*array_data[i][9]
-		index_array=[0]*len(faceList)
+
 		for f in range(len(faceList)):
-			
 			#созаем пустой массив для временной обработки
-			rectangle_now=np.zeros((array_data[i][9],3))
-			
+			rectangle_now=np.zeros((array_data[i][9],3))	
 			#перебор всех следующих элементов i-й и все лица что на этом кадре
 			for rect in range(array_data[i][9]):
 				x2=array_data[i+rect][1]
@@ -99,33 +97,24 @@ while i < len(array_data):
 				rectangle_now[rect][2]=dist
 			#поиск минимального расстояния
 			minimym, index = index_min(rectangle_now, 2)
-			index_array[f]=index
 			#обновление массива
 			metka[index]=True
 					
 			faceList[f][1]=rectangle_now[index][0]
 			faceList[f][2]=rectangle_now[index][1]
 			array_data[i+index][10]=faceList[f][10]
-			faceList[f][5]=array_data[i+rect][5] # обновление времени
-			
-
-
+			faceList[f][5]=array_data[i+index][5] # обновление времени
 
 		for k in range(array_data[i][9]):
-			#if (array_data[i+k][10]=="none"):
 			if (metka[k]==False):
 				array_data[i+k][10]=faceCount
-				faceList.append(array_data[i+k])
+				faceList.append(copy.deepcopy(array_data[i+k]))
 				faceCount += 1
-
-		for k in range(len(faceList)):
-			print('-----------------------',faceList[k][10])		
+	
 #3
 	if(len(faceList) > array_data[i][9]):
-		print('3-----------------------------3')
 		metka=[0]*len(faceList)
 		for rect in range(array_data[i][9]):
-			#index_array=[0]*len(faceList)
 			rectangle_now=np.zeros((len(faceList),3))
 			for f in range(len(faceList)):
 		
@@ -142,17 +131,11 @@ while i < len(array_data):
 			#поиск минимального расстояния
 			minimym, index = index_min(rectangle_now, 2)
 			#обновление массива
-					
 			faceList[rect][1]=rectangle_now[index][0]
 			faceList[rect][2]=rectangle_now[index][1]
-		
-			faceList[rect][5]=array_data[i+rect][5] # обновление времени
-			#index_array[rect]=index
-			
+			faceList[rect][5]=array_data[i+index][5] # обновление времени	
 			array_data[i+rect][10]=faceList[index][10]		
 			metka[index]=True
-
-			#print(len(faceList),array_data[i][9])
 
 		n=0
 		while n < len(faceList):
@@ -161,12 +144,7 @@ while i < len(array_data):
 			n+=1
 	i+=array_data[i][9]
 
-	for k in range(len(faceList)):
-		print('face',faceList[k][10])
 
-
-array_finish_list=[]
-ID=0
 for i in range(len(array_data)):
 	if (i==0):
 		array_data[i][0]=0
@@ -175,19 +153,31 @@ for i in range(len(array_data)):
 	if (array_data[i][0]=="none"):
 		array_data[i][0]=ID
 		ID=ID+1
-
+	
 	for k in range(len(array_data)-i-1):
-
-		if (array_data[k+i+1][0]=="none"):
-			dist=distance.euclidean(array_data[i][6], array_data[k+i+1][6])
-			array_data[i+k+1][8]=dist
-
-			
-			if (dist<=0.5 	):
-				array_data[k+i+1][0]=array_data[i][0]
-				#print('ID',array_data[k+i+1][0])
 		
-				
+		namber_k=array_data[k+i+1][10]
+		if (array_data[k+i+1][0]=="none"):
+			n_array=[]
+			array_namber_k=[]			
+			
+			for n in range(len(array_data)-i-k-1):
+				if (array_data[k+i+n+1][10]==namber_k):
+					n_array.append(n)				
+					dist=distance.euclidean(array_data[i][6], array_data[k+i+n+1][6])
+					array_data[i+k+n+1][8]=dist
+					list_1=copy.deepcopy(array_data[k+i+n+1])
+					array_namber_k.append(list_1)
+			
+			#if len(array_namber_k)>0:
+			minimym, index = index_min(array_namber_k,8)
+			if (minimym<=0.5):
+				print('minim',minimym)
+				for d in range(len(array_namber_k)):
+					#array_data[k+i+1][0]=array_data[i][0]
+					array_data[k+i+n_array[d]+1][0]=array_data[i][0]
+					print(array_data[k+i+n_array[d]+1][0],i)					
+					#array_namber_k[d][0]=array_data[i][0]
 
 for i in range(len(array_data)):
 	#проверка на метку
@@ -224,7 +214,7 @@ for i in range(len(array_data)):
 			array_finish_list.append(array_f[0])
 
 				
-
+				
 
 
 
